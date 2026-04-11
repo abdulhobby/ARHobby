6c172de8f95577ba198fd8ee70891cbb71429865
abdul8840 <abdul14941naseer@gmail.com>
2026-04-11 17:19:00 +0530

    fix cookie

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -14,8 +14,8 @@ export const sendTokenResponse = (user, statusCode, res, type = 'user') => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: isProd,        // ✅ ALWAYS TRUE
-    sameSite: "None"       // ✅ ALWAYS NONE
+    secure: true,        // 🔥 FIXED
+    sameSite: "None"     // 🔥 FIXED
   };
 
   const userData = {
